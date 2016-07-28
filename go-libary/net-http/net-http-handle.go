344d0e6a2fcc22a56850841dc32f354b90e74477
luodaokai <guijihe@163.com>
2016-07-28 16:59:36 +0800

    http.FileServer(http.Dir)

--- go-libary/net-http/net-http-handle.go
+++ go-libary/net-http/net-http-handle.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	http.Handle("/test", http.FileServer(http.Dir("./")))
 	http.Handle("/download/", http.StripPrefix("/download/", http.FileServer(http.Dir("./"))))
-	http.Handle("/tmpfile", http.StripPrefix("/tmpfiles/", http.FileServer(http.Dir("/tmp"))))
-	http.ListenAndServe(":9999", nil)
+	http.Handle("/code/", http.StripPrefix("/code/", http.FileServer(http.Dir("/Users/path/code")))) // 会自动去找目录下的index.html文件并且渲染,如果没有的话，就显示目录文件
+	http.ListenAndServe(":9998", nil)
 
 }
